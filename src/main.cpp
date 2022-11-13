f20f3516aba39c4f795a3d44038f5826a22487ac
astral-shining <astral-shining@users.noreply.github.com>
2022-11-14 00:43:34 +0100

    Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@ in vec4 a_color;
 
 out vec4 color;
 
-uniform mat3 model;
-
 void main() {
     color = a_color;
-    gl_Position = vec4(model * a_pos, 1.0);
+    gl_Position = vec4(a_pos, 1.0);
 }
 )",
 R"(
@@ -27,7 +25,7 @@ void main() {
 }
 )"
 };
-#include <glm/gtx/matrix_transform_2d.hpp>
+
 int main() {
     GL::VAO vao;
     vao.use();
@@ -42,7 +40,6 @@ int main() {
     s.attribLinker(vbo)
     .autoLinkAll();
     vao.unuse();
-    glm::mat3 m { 0.5f };
 
     float x {};
     while (window.update()) {
@@ -52,7 +49,6 @@ int main() {
         vbo.bufferSubData(GL::HSVA(x+180.f, 1.f, 1.f), 2);
         vbo.bufferSubData(GL::HSVA(x+270.f, 1.f, 1.f), 3);
         s.use();
-        s.uniform("model", glm::rotate(m, x/32.f));
      
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
