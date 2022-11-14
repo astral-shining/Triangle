20da81a24653f341c88cb0cae9500b48dec158f6
astral-shining <astral-shining@users.noreply.github.com>
2022-11-14 01:44:45 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@ in vec4 a_color;
 
 out vec4 color;
 
-uniform mat3 model;
-
 void main() {
     color = a_color;
-    gl_Position = vec4(a_pos * model, 1.0);
+    gl_Position = vec4(a_pos, 1.0);
 }
 )",
 R"(
